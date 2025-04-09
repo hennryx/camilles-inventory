407253fb7842e17498c2a1a7cf82bc869c560855
Hennryx Samson <118619658+hennryx@users.noreply.github.com>
2025-04-09 19:58:45 +0800

    mongodb connection

--- index.js
+++ index.js
@@ -1,5 +1,50 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const dotenv = require('dotenv');
+require('dotenv').config();
+const path = require('path')
+const app = express();
+/* const routes = require('./routes'); */
 
+const cors = require('cors');
+const cookieParser = require('cookie-parser')
+
+const connectDB = require('./config/db')
+connectDB()
+    .then(() => {
+        const corsConfig = {
+            origin: 'http://localhost:3000',
+            methods: ["GET", "POST", "PUT", "DELETE"],
+            credentials: true,
+            allowedHeaders: [
+                "Origin",
+                "Content-Type",
+                "X-Requested-With",
+                "Accept",
+                "Authorization",
+            ],
+        };
+
+        app.use(cors(corsConfig));
+
+        app.use(
+            express.urlencoded({
+                extended: true,
+                limit: "50mb",
+            })
+        );
+
+        app.use(cookieParser())
+        app.use(express.json({ limit: "50mb" }));
+        app.use('/assets', express.static(path.join(__dirname, 'assets')));
+
+        /* // routes
+        app.use('/', routes); */
+
+
+        const port = process.env.PORT || 5000;
+        app.listen(port, () => {
+            console.log(`Server is running on port ${port}`);
+        });
+
+    }).catch((err) => {
+        console.log(err);
+    });
