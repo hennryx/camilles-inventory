74450adfeb6cc141675be9ea1fea342bd049aec3
Hennryx Samson <118619658+hennryx@users.noreply.github.com>
2025-05-13 19:34:44 +0800

    update index

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,8 @@ import useAuthStore from './services/stores/authStore';
 import NotFound from './pages/notFound';
 import HeroPage from './pages/home';
 import Views from './pages/views';
+import ResetPassword from './pages/resetPassword';
+import ForgotPassword from './pages/forgotPassword';
 
 function App() {
     const { auth, token, validateToken } = useAuthStore();
@@ -28,6 +30,8 @@ function App() {
     return (
         <>
             <Routes>
+                <Route path="/reset-password/:id" element={<ResetPassword />} />
+                <Route path="/forgot-password" element={<ForgotPassword />} />
                 {heroPaths.includes(urlPath) 
                 ? <Route path='/*' element={<HeroPage />} /> 
                 : <Route path='/*' element={<Views />} />
